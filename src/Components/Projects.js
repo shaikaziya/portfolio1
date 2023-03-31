dc7e7cc3c7aa13fd5e8dec174a5416859feab212
shaikaziya <shaikaziya1234@gmail.com>
2023-03-31 21:02:53 +0530

    added image link

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -166,7 +166,7 @@ function Projects() {
 
             <div class="col-lg-4 col-md-4 col-sm-4">
               <div class="insidebox text-center">
-                <img className="img" src="https://tse3.mm.bing.net/th?id=OIP.qK9F1E1iB3piswkWTBWRPwHaEK&pid=Api&P=0" alt={project1} />
+                <img className="img" src="https://tse2.mm.bing.net/th?id=OIP.VdCvizZFqi0IRCBi9XDFpQHaE8&pid=Api&P=0" alt={project1} />
                 <h5>E-Commerce Cart</h5>
                 
                 <div className="card-footer">
